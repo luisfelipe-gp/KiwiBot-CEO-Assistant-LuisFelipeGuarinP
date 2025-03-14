f27d842996efdb31beba91601fc5b36cb56075aa
luisfelipegp <63433752+luisfelipe-gp@users.noreply.github.com>
2025-03-13 23:55:55 -0500

    Ajustes config y package

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/KiwiBot-CEO-Assistant-LuisFelipeGuarinP",
+  /* basePath: "/KiwiBot-CEO-Assistant-LuisFelipeGuarinP",
   output: "export",  // <=== enables static exports
-  reactStrictMode: true
+  reactStrictMode: true */
 };
 
 export default nextConfig;
